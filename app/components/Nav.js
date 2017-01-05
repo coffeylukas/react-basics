ec20ec150d3f9e237fd1029663396a73dd933f04
Lukas Coffey <coffeylukas@gmail.com>
2017-01-05 01:30:15 -0500

    added webpack, components, and index.html, as well as dist/ folder

---
+++ app/components/Nav.js
@@ -0,0 +1,14 @@
+// include React
+import React from 'react';
+
+var Nav = React.createClass({
+  render: function() {
+    return(
+      <div className="nav">
+        <h3>Welcome to React!</h3>
+      </div>
+    )
+  }
+});
+
+module.exports = Nav;
---
+++ app/index.js
@@ -0,0 +1,7 @@
+// include React
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Nav from './components/Nav.js';
+
+ReactDOM.render(<Nav />, document.getElementById('application'));
